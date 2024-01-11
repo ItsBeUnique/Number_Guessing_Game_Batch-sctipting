05f611925b9ab73b0645612d7e51b0d878e37914
ItsBeUnique <102958237+ItsBeUnique@users.noreply.github.com>
2024-01-12 00:36:43 +0530

    game file added

---
+++ Hacker_Terminal.bat
@@ -0,0 +1,6 @@
+@echo off
+:again
+color a
+echo %RANDOM%  %RANDOM%  %RANDOM%  %RANDOM%  %RANDOM%  %RANDOM%  %RANDOM%  %RANDOM%  %RANDOM%  %RANDOM%  %RANDOM%  %RANDOM%  %RANDOM%  %RANDOM%  %RANDOM%  %RANDOM%  %RANDOM% 
+goto:again
+pause
